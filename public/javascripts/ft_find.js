f46ae2e97715ff8a159e6c179af5b42b664213ce
Ubuntu <ubuntu@ip-172-31-18-134.us-west-2.compute.internal>
2017-08-03 04:39:49 +0000

    주석추가 - post

--- public/javascripts/ft_find.js
+++ public/javascripts/ft_find.js
@@ -1,5 +1,58 @@
 
 $(document).ready(function() {
+  /*
+  $('#find_button').click(function(){
+      var f_name = $('#f_name').val();
+      var f_number = $('#f_number').val();
+      var data = {
+        'f_name': f_name,
+        'f_number' : f_number
+      }
+      $.ajax({
+        type: "post",
+        url: "/first_test/findID_success",
+        datatype: "json", // expecting JSON to be returned
+        data: data,
+        cache:false,
+        success: function(result) {
+          if(result == 'error'){
+            alert('잘못된 유저정보');
+          }else{
+            $(window).attr('location','/first_test/findID_success');
+          }
+        },
+        error: function(error) {
+          alert(error);
+        }
+      })//ajax
+  });
 
-
+  $('#find_button2').click(function(){
+    var f_name = $('#f_name').val();
+    var f_number = $('#f_number').val();
+    var f_email = $('f_email').val();
+    var data = {
+      'f_email' : f_email,
+      'f_name': f_name,
+      'f_number' : f_number
+    }
+      $.ajax({
+        type: "post",
+        url: "/first_test/findpwd_success",
+        datatype: "json", // expecting JSON to be returned
+        data: data,
+        cache:false,
+        success: function(result) {
+          if(result == 'error'){
+            alert('잘못된 유저정보');
+          }else{
+            $(window).attr('location','/first_test/findpwd_success');
+          }
+        },
+        error: function(error) {
+          alert(error);
+        }
+      })//ajax
+  });
+*/
 });
